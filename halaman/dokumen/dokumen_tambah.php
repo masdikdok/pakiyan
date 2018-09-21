5221e1868059072fb1f176fbb986aaff5cd922d7
dickiandrea <dicki123andrea@gmail.com>
2018-09-21 16:32:49 +0700

    Update

--- halaman/dokumen/dokumen_tambah.php
+++ halaman/dokumen/dokumen_tambah.php
@@ -26,10 +26,10 @@
 
 <?php
 
-if(isset($_POST['BtnAdd'])){
-    $judul = $_POST['judul'];
+if(isset(Input::get('BtnAdd'))){
+    $judul = Input::get('judul');
     $filename = $_FILES['isiberita']['name'];
-    $lokasi = 'files/'.$filename;
+    $lokasi = '../pusatdata/file/'.$filename;
     move_uploaded_file($_FILES['isiberita']['tmp_name'],$lokasi);
 
     function parseWord($userDoc)
@@ -52,47 +52,32 @@ if(isset($_POST['BtnAdd'])){
     }
 
     $userDoc = $lokasi;
-
     $string = parseWord($userDoc);
-
     $idberita = str_replace(" ", "_", $judul); // replace spasi dgn '_' utk dijadikan id_berita
-    // masukkan ke database
-            $query = "INSERT INTO tbberita VALUES ('','$idberita','$judul','$string','$lokasi')";
-            $q_insert = mysql_query($query)or die (mysql_error());
-            //if ($q_insert) {
-            //  move_uploaded_file($_FILES['isiberita']['tmp_name'],$lokasi);
-        //  }
 
-    include "preproses.php"; // ambil script preproses.php
-    include ('Algoritma_stemming.php'); // ambil script stemming-nazief-adriani.php
+    $field = array(
+      'id_berita' => $idberita,
+      'Judul' => $judul,
+      'Berita' => $string,
+      'url' => $lokasi,
+    );
 
-    // ambil keyword berdasarkan berita yg baru dimasukkan ($idberita = judul berita)
-    $sql5 = mysql_query("SELECT * FROM tbkeyword where id_berita = '$idberita' order by frekuensi DESC");
-    while ($stemming = mysql_fetch_array($sql5)){
-        if (!empty($stemming['kata'])){
-            $teks5 = strtolower($stemming['kata']); // ubah ke huruf kecil semua
-
-            // hapus tanda baca dan angka dengan spasi
-            $tokenKarakter=array('’','—',' ','/',',','?','.',':',';',',','!','[',']','{','}','(',')','-','_','+','=','<','>','\'','"','\\','@','#','$','%','^','&','*','`','~','0','1','2','3','4','5','6','7','8','9','â€','”','“');
-            $teks5= str_replace($tokenKarakter,' ',$teks5);
-            $split = explode(' ',$teks5); // pisahkan kata yg ada spasinya
-
-            foreach($split as $key5=>$kata){ // utk setiap kata yg telah dilakukan di atas, lakukan fungsi Nazief_Adriani
-                $hasil_stemming = ECS(trim($kata));
-                // masukkan ke dalam database tbstem
-                mysql_query("INSERT INTO tbstem VALUES ('', '$stemming[id_berita]', '$stemming[kata]', '$hasil_stemming')");
-            }
-        }
+    $result = $allquery->tambah('tbberita', $field);
+    if($result){
+      echo "
+        <script type='text/javascript'>
+          alert('Tambah data berita BERHASIL!');
+          document.location = '?page=dokumen&subpage=dokumen_tambah';
+        </script>
+      ";
+    }else {
+      echo "
+        <script type='text/javascript'>
+          alert('GAGAL menambahkan berita baru!');
+          document.location = '?page=dokumen&subpage=dokumen_tambah';
+        </script>
+      ";
     }
-    // tampilkan hasil stemming keyword
-    echo "<h3>Hasil Stemming Keyword:</h3>";
-    echo "<table class='display table table-bordered table-striped' id='example'>";
-    echo "<tr><th>Kata</th><th>Stemming</th></tr>";
-    $sql6 = mysql_query("SELECT * FROM tbstem where id_berita = '$idberita' order by Id");
-    while ($stemming = mysql_fetch_array($sql6)){
-        echo "<tr><td>$stemming[Term]</td><td>$stemming[kata]</td></tr>";
-    }
-    echo "</table>";
 
 }
 ?>
